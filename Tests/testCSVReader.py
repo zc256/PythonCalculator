345f5ce6279dca25bfb8f189973963ca3b576561
Zach <zc256@njit.edu>
2021-03-17 22:28:35 -0400

    Task: Added new test data and test cases for CSV read

---
+++ Tests/testCSVReader.py
@@ -0,0 +1,22 @@
+import unittest
+from CsvReader.CsvReader import CsvReader, ClassFactory
+from pprint import pprint
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.csv_reader = CsvReader('Tests/Data/testJobs.csv')
+
+    def test_return_as_objects(self):
+        staff = self.csv_reader.return_data_as_class('job')
+        self.assertIsInstance(staff, list)
+        test_class = ClassFactory('job', self.csv_reader.data[0])
+
+        for job in staff:
+            self.assertEqual(job.__name__, test_class.__name__)
+            pprint(vars(staff))
+
+	
+
+
+if __name__ == '__main__':
+    unittest.main()
